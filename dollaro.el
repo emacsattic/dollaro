d18b07fc6a63ce773b745714e135284b6eec7485
Jonas Bernoulli <jonas@bernoul.li>
2026-02-10 16:14:27 +0100

    Bump version

--- dollaro.el
+++ dollaro.el
@@ -5,7 +5,7 @@
 ;; Author: Alessandro Piras <laynor@gmail.com>
 ;; Keywords: tools, convenience
 ;; Package-Requires: ((s "1.6.0"))
-;; Version: 0.2
+;; Version: 0.2.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
